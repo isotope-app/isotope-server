35269474d0a99fc494615c42134620b4a2646ed8
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-09-21 21:26:07 -0400

    adds hot reloading for develop docker compose!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use listenfd::ListenFd;
+
 
 fn index(req: HttpRequest) -> &'static str {
     println!("REQ: {:?}", req);
     "Hello world!"
 }
 
-fn main() -> std::io::Result<()> {
+fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-
-    HttpServer::new(|| {
+    
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
             .service(web::resource("/index.html").to(|| "Hello world!"))
             .service(web::resource("/").to(index))
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
+    });
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l).unwrap()
+    }else{
+        server.bind("0.0.0.0:8080").unwrap()
+    };
+    server.run().unwrap()
 } 
