ba9b545610bad5989fa14f5ac8515e1bc087c7bf
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-10-04 23:15:00 -0400

    db connection (#2)
    * fixes "fixed" docker things 
    * allows db to connect to actix
    * sets up error handling

---
+++ src/app/users.rs
@@ -0,0 +1,31 @@
+use actix_web::{HttpRequest, HttpResponse}
+use regex::Regex; 
+use validator::Validate;
+
+lazy_static! {
+	static ref RE_USERNAME: Regex::new(r"^[_0-9a-zA-z]+$").unwrap();
+}
+
+#[derive(Debug,Validate,Deserialize)]
+pub struct RegisterUser {
+	#[validate(
+		length(
+			min = "1",
+			max = "25",
+			message = "must be 1-20 characters long"
+		)
+	),
+	regex(
+		path = "RE_USERNAME",
+		message = "alphanumeric/underscore only"
+	)]
+	pub username: String, 
+	#[validate(email(message = "not a valid email address"))]
+	pub email: String 
+	#[validate(length(
+		min = "8"
+		message = "fails validation, must be at least 8 characters long"
+	))]
+	pub password: String,
+	
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@
 extern crate tera;
 #[macro_use]
 extern crate dotenv_codegen;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate serde_json;
 extern crate dotenv;
 
+mod db;
+mod error;
+mod prelude;
+
+use crate::db::{new_pool, DbExecutor};
 
 use actix_files as fs;
-use tera::Tera;
+use actix::prelude::{SyncArbiter};
 use actix_web::{middleware, web, App, HttpServer};
+use tera::Tera;
 use listenfd::ListenFd;
 use dotenv::dotenv;
+use std::env;
 
 mod api;
 
@@ -17,8 +28,16 @@ fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     dotenv().ok();
+ 	let sys = actix::System::new("conduit");
 
+	let database_url = dotenv!("DATABASE_URL");
+
+	let database_pool = new_pool(database_url).expect("Failed to create pool");
+ 	let _database_address = SyncArbiter::start(num_cpus::get(), move || DbExecutor(database_pool.clone()));
+
+	let bind_address = env::var("BIND_ADDRESS").expect("BIND ADDRESS is not set");
     let mut listenfd = ListenFd::from_env();
+
     let mut server = HttpServer::new(|| {
         let templates: Tera = compile_templates!("templates/**/*");
         App::new()
@@ -32,8 +51,9 @@ fn main() {
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
     }else{
-        server.bind("0.0.0.0:8080").unwrap()
+        server.bind(&bind_address).unwrap()
     };
+
 
     server.run().unwrap()
 } 
