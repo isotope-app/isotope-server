0e49eb11896387f7a1a7865169c5daadb8f2987f
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-10-05 16:20:38 -0400

    Code cleanup
    * cleans up some code
    
    * changes a few things so as not to conflict with diesel
    
    * fixes small things

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,25 @@ extern crate dotenv;
 mod db;
 mod error;
 mod prelude;
+mod api;
 
-use crate::db::{new_pool, DbExecutor};
+use crate::db::{db_conn::new_pool, db_conn::DbExecutor};
+use crate::api::{routes::routes};
 
-use actix_files as fs;
-use actix::prelude::{SyncArbiter};
-use actix_web::{middleware, web, App, HttpServer};
+use actix::prelude::{Addr, SyncArbiter};
+use actix_web::{
+	middleware, 
+	App, 
+	web::Data,
+	HttpServer};
 use tera::Tera;
 use listenfd::ListenFd;
 use dotenv::dotenv;
 use std::env;
 
-mod api;
+pub struct AppState {
+    pub db: Addr<DbExecutor>,
+}
 
 fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
@@ -30,30 +37,32 @@ fn main() {
     dotenv().ok();
  	let sys = actix::System::new("conduit");
 
-	let database_url = dotenv!("DATABASE_URL");
-
+	let database_url = dotenv!("MYSQL_DATABASE_URL");
 	let database_pool = new_pool(database_url).expect("Failed to create pool");
- 	let _database_address = SyncArbiter::start(num_cpus::get(), move || DbExecutor(database_pool.clone()));
-
+ 	let database_address = SyncArbiter::start(num_cpus::get(), move || DbExecutor(database_pool.clone()));
 	let bind_address = env::var("BIND_ADDRESS").expect("BIND ADDRESS is not set");
     let mut listenfd = ListenFd::from_env();
 
-    let mut server = HttpServer::new(|| {
+    let mut server = HttpServer::new( move || {
+		let state = AppState {
+			db: database_address.clone(),
+		};
+		
         let templates: Tera = compile_templates!("templates/**/*");
         App::new()
-            // enable logger
+			.register_data(Data::new(state))
             .data(templates)
             .wrap(middleware::Logger::default())
-            .service(web::resource("/").to(api::index))
-            .service(fs::Files::new("/static", "static/"))
+			// the routes are in api/routes
+			.configure(routes)
     });
-
+	// this allows hot reloading in dev mode 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
     }else{
         server.bind(&bind_address).unwrap()
     };
 
-
-    server.run().unwrap()
+    server.run().unwrap();
+	let _ = sys.run();
 } 
