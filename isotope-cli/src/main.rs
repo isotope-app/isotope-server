4065f1d5ebb7f124ce8b856873b5af73212e8b35
Skyler Jane Clark <skylerjaneclark@gmail.com>
2019-10-06 14:41:13 -0400

    adds navbar

---
+++ isotope-cli/src/main.rs
@@ -0,0 +1,16 @@
+use clap::App;
+
+fn main(){
+	let mut app = App::new("Isotope CLI")
+		.bin_name("isotope")
+		.version(env!("CARGO_PKG_VERSION"))
+		.about("A collection of tools to manage your Isotope instance")
+		.subcommand(users::command());
+	let matches = app.clone().get_matches();
+	
+	match matches.subcommand(){
+		("users", Some(args))=>{
+			!println("users commands")
+		}
+	}
+}
