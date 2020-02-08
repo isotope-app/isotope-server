8ca7a850dff23809f4bcb19a3672a3c416973409
Skyler Jane Clark <skylerjaneclark@gmail.com>
2020-02-08 17:53:13 -0500

    fixes hot reloading ;)

--- src/main.rs
+++ src/main.rs
@@ -35,19 +35,21 @@ fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     dotenv().ok();
- 	let sys = actix::System::new("conduit");
+
+// I genuinely do not know what this does but without it it panics 
+// because the system isn't running :(
+ 	let _sys = actix::System::new("conduit");
 
 	let database_url = dotenv!("MYSQL_DATABASE_URL");
 	let database_pool = new_pool(database_url).expect("Failed to create pool");
  	let database_address = SyncArbiter::start(num_cpus::get(), move || DbExecutor(database_pool.clone()));
 	let bind_address = env::var("BIND_ADDRESS").expect("BIND ADDRESS is not set");
-    let mut listenfd = ListenFd::from_env();
 
+    let mut listenfd = ListenFd::from_env();
     let mut server = HttpServer::new( move || {
 		let state = AppState {
 			db: database_address.clone(),
 		};
-		
         let templates: Tera = compile_templates!("templates/**/*");
         App::new()
 			.register_data(Data::new(state))
@@ -59,10 +61,10 @@ fn main() {
 	// this allows hot reloading in dev mode 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
+
     }else{
         server.bind(&bind_address).unwrap()
     };
 
     server.run().unwrap();
-	let _ = sys.run();
 } 
