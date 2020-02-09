7c6f0c7640aae2405ff0d26d645701a9055fad2f
Skyler Jane Clark <skylerjaneclark@gmail.com>
2020-02-08 22:46:22 -0500

    working on getting the cli working!

--- deployments/cli-docker-entrypoint.sh
+++ deployments/cli-docker-entrypoint.sh
@@ -6,4 +6,4 @@ echo "installing picopik cli....."
 cd /picopik/picopik-server/picopik-cli
 cargo watch -s "cargo install --path ." 
 
-exec "$@"
+exec "$@"l
--- picopik-cli/src/main.rs
+++ picopik-cli/src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 use picopik_models::db;
 
 fn main(){
-	let mut app = App::new("picopik CLI")
+    let mut app = App::new("picopik CLI")
 		.bin_name("picopik")
 		.version(env!("CARGO_PKG_VERSION"))
 		.about("A collection of tools to manage your picopik instance")
@@ -13,11 +13,13 @@ fn main(){
 	let matches = app.clone().get_matches();
     
     let sys = actix::System::new("picopik-cli");
+
     let database_url = env::var("MYSQL_DATABASE_URL").expect("should return the mysql databse");
     let db = db::start_db(database_url);
    
 	match matches.subcommand(){
 		("users", Some(args))=>{
+            print!("hellos")
 			users::run(args, db)
 		}
 		 _ => app.print_help().expect("Couldn't print help"),
